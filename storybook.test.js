b50de70edb0da769ce5cc9a2c15d4e09949ade69
Alan Escobedo <alaanprogramacion@gmail.com>
2021-10-08 16:32:19 -0500

    added global mock of window for jest tests

--- storybook.test.js
+++ storybook.test.js
@@ -1,2 +1,8 @@
 import initStoryshots from '@storybook/addon-storyshots';
+
+//  Created a global mock to avoid the error for the tests:
+//  --- TypeError: Cannot read property 'STORYBOOK_HOOKS_CONTEXT' of undefined ---
+//  https://github.com/storybookjs/storybook/issues/8083#issuecomment-533184737
+jest.mock('global', () => Object.assign(global, { window: { STORYBOOK_HOOKS_CONTEXT: '' } }));
+
 initStoryshots();
