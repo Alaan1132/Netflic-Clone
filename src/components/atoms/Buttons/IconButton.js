58c2185359bb5fbd9e24fdd1937dc3cd09b3f8e8
Alan Escobedo <alaanprogramacion@gmail.com>
2021-10-10 18:11:48 -0500

    IconButton Component created with storyshots

---
+++ src/components/atoms/Buttons/IconButton.js
@@ -0,0 +1,33 @@
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+import { Icon } from '../Icons';
+import { options } from '../../../constants';
+
+import styles from './Button.module.css';
+
+export const IconButton = ({ label, type, size, iconName, iconColor, isRounded, ...props }) => {
+  return (
+    <button
+      className={classNames(styles.btn, {
+        [styles[type]]: type,
+        [styles[`size-${size}`]]: size,
+        [styles.isRounded]: isRounded,
+      })}
+      {...props}>
+      {label}
+      <Icon name={iconName} size={size} color={iconColor} />
+    </button>
+  );
+};
+
+IconButton.propTypes = {
+  label: PropTypes.string,
+  type: PropTypes.oneOf(options.types).isRequired,
+  isRounded: PropTypes.bool,
+};
+
+IconButton.defaultProps = {
+  type: 'primary',
+  isRounded: false,
+};
